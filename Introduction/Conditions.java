b3af6e6983cf3c6acd54310244b2132ffaa73fb0
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-05-25 21:35:55 +0530

    feat: Add Conditions class for handling age conditions and printing name multiple times

---
+++ Introduction/Conditions.java
@@ -0,0 +1,16 @@
+public class Conditions {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("You can vote now");
+        } else {
+            System.out.println("You cannot vote now");
+        }
+        // print your name 5 times usinga count variable
+        int count = 1;
+        while(count != 6){
+            System.out.println("Ankit Mishra");
+            count++;
+        }
+    }
+}
--- Introduction/TypeConversion.java
+++ Introduction/TypeConversion.java
@@ -3,11 +3,29 @@ public class TypeConversion {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         float marks = input.nextFloat();
-        int marksInt = (int) marks;
-        System.out.println(marksInt);
+        // int marksInt = (int) marks;
+        // System.out.println(marksInt);
         // now again converting it into flaot
-        System.out.println("Afte converting it into float: " + (float)marksInt);
-        float marksFloat = (float)marks;
-        System.out.println(marksFloat);
+        // System.out.println("Afte converting it into float: " + (float)marksInt);
+        // float marksFloat = (float)marks;
+        // System.out.println(marksFloat);
+        // java performs automatic type conversion
+        // int to float
+        // int a = 10;
+        // float b = a;
+        // System.out.println(b);
+        
+        // this is called type casting
+        // type casting is of two types
+        // 1. Implicit type casting
+        // 2. Explicit type casting
+        // Implicit type casting
+        // byte -> short -> int -> long -> float -> double
+        // Explicit type casting
+        // double -> float -> long -> int -> short -> byte
+        
+        // float c = 20.5f;
+        // int d = (int)c;
+        // System.out.println(d);
     }
 }
