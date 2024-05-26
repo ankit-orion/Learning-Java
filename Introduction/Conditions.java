b28ce8c997d29b334d4025e58440520ac64786a0
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-05-26 09:43:30 +0530

    chore: Remove commented code and add Loops class for printing name multiple times

--- Introduction/Conditions.java
+++ Introduction/Conditions.java
@@ -1,16 +1,17 @@
 public class Conditions {
     public static void main(String[] args) {
-        int age = 18;
-        if (age >= 18) {
-            System.out.println("You can vote now");
-        } else {
-            System.out.println("You cannot vote now");
-        }
+        // int age = 18;
+        // if (age >= 18) {
+        //     System.out.println("You can vote now");
+        // } else {
+        //     System.out.println("You cannot vote now");
+        // }
         // print your name 5 times usinga count variable
-        int count = 1;
-        while(count != 6){
-            System.out.println("Ankit Mishra");
-            count++;
-        }
+        // int count = 1;
+        // while(count != 6){
+        //     System.out.println("Ankit Mishra");
+        //     count++;
+        // }
+        
     }
 }
