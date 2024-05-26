3857286b37d40fc1459d1ddda049ba8ea6dd0ea1
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-05-26 21:25:07 +0530

    feat: Add declaration of arrays and iteration over it

---
+++ Arrays/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    // storing 5 roll numebrs using arrays
+    // sytax:
+    // datatype[] arraysName = new datatype[size];
+    public static void main(String[] args) {
+        // int[] rollNumbers = new int[5];
+        // Scanner in = new Scanner(System.in);
+        // for(int i = 0; i < rollNumbers.length; i++){
+        //     rollNumbers[i] = in.nextInt();
+        // }
+        // System.out.println("Array elements are: ");
+        // for(int i = 0; i < rollNumbers.length; i++){
+        //     System.out.print(rollNumbers[i] +" ");
+        // }
+
+        // now inc ase the user wants to get the nuber of elements we can take it from the input
+        // Scanner in = new Scanner(System.in);
+        // System.out.print("Enter the number of elements: ");
+        // int n = in.nextInt();
+    }
+}
