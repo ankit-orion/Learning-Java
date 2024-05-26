57ad51a97e97957df066ed0d08887ef96962fd6a
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-05-26 09:59:39 +0530

    feat: Add user input and check for alphabet in CheckAlphabet class

--- Introduction/CheckAlphabet.java
+++ Introduction/CheckAlphabet.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class CheckAlphabet {
-    
+    public static void main(String[] args) {
+        // take a character as input from the user using scanner class
+        Scanner in = new Scanner(System.in);
+        char ch = in.next().charAt(0);
+        System.out.println(ch);
+        if(ch >= 'a' && ch <= 'z'){
+            System.out.println("Lowercase");
+        }
+        else if(ch >= 'A' && ch <= 'Z'){
+            System.out.println("Uppercase");
+        }
+        else{
+            System.out.println("Not an alphabet");
+        }
+    }
 }
