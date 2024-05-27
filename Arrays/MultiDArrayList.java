85c73c96f8f20cec307dd142ad283dfb3b7f8546
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-05-27 20:16:03 +0530

    Add input and printing functionality to MultiDArrayList class

--- Arrays/MultiDArrayList.java
+++ Arrays/MultiDArrayList.java
@@ -6,5 +6,14 @@ public class MultiDArrayList {
         for(int i = 0; i < 3; i++){
             arr.add(new ArrayList<Integer>());
         }
+        // taking input in 2d array list
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                arr.get(i).add(i+j);
+            }
+        }
+        for(int i: arr.get(0)){
+            System.out.print(i +" ");
+        }
     }
 }
